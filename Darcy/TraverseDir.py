5a07b9c1b5b276d3ad6ac0e509f9c3b850f24a66
Negar Ghorbani <negargh@ics.uci.edu>
2019-12-29 16:55:21 -0800

    TraverseDir fixed to work with all paths

--- Darcy/TraverseDir.py
+++ Darcy/TraverseDir.py
@@ -1,11 +1,12 @@
 import os
 import sys
 import subprocess
+
+count = 0
 for root, dirs, files in os.walk(sys.argv[1]):
     for file in files:
         if file.endswith(".class"):
         	if not file.startswith("module-info"):
-        		# print(root)
-        		i = root.split('/')[10]+'-'+file.split('.')[0]
+        		i = str(count)+'-'+file.split('.')[0]
+        		count = count+1
         		subprocess.call(['java', '-jar', 'Classycle1.4.2/classycle.jar', '-xmlFile=XMLreports/'+str(i)+'.xml', os.path.join(root, file)])
-				#print(os.path.join(root, file))
